2c0190a8dc2cc0477e6b871b02c6aa9c168d9a79
Iceprowin <127022068+Iceprowin@users.noreply.github.com>
2025-09-28 11:31:19 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,11 @@
 session_start();
 
 $connection = mysqli_connect(
-    "localhost",
-    "root",
-    "root",
-    "tuckshop"
+    "localhost", //因數據庫而異
+    "root", //因數據庫而異
+    "root", //因數據庫而異
+    "tuckshop" //因數據庫而異
 );
 if(!$connection) die("Cannot connect to DB");
+
 ?>
